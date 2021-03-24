b8bbd87c26a915e13f95d90a20fe179fc83fe7ef
Carlos Mario Fonseca Naranjo <carlfona@suramericana.com.co>
2021-03-23 21:22:16 -0500

    first commit

---
+++ src/main/java/User.java
@@ -0,0 +1,39 @@
+public class User {
+    private String firstName;
+    private String lastName;
+    private String age;
+    private Address address;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+}
